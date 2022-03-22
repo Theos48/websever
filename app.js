5ae1ccff02e54187ef06614e49070598252a9367
Theos48 <david_ramirezz@hotmail.com>
2022-03-21 20:43:09 -0600

    redenrizado de paginas desde el servidor

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const port = 3000;
+
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//servir contenido statico
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'David De Paz Ramirez',
+        curso: 'Curso de NodeJS'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'David De Paz Ramirez',
+        curso: 'Curso de NodeJS'
+    });
+});
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'David De Paz Ramirez',
+        curso: 'Curso de NodeJS'
+    });
+});
+
+app.get('*', (req, res) => { 
+    res.sendFile( __dirname + '/public/404.html')
+});
+app.listen(port, () => {
+    console.log(`El ejemplo esta corriendo en el puerto ${port}`);
+});
