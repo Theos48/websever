885fab4a769fea24de6aac9aac4cfbd95e2cbdb5
Theos48 <david_ramirezz@hotmail.com>
2022-03-26 17:59:12 -0600

    agregado dotenv para gestionar el puerto

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
+
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 
 // Handlebars
